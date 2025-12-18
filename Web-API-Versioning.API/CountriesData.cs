83ce82479746200cd8d55bb1f142fc4ce31e9409
CS150538 <chanduvcs538@gmail.com>
2025-12-18 15:30:29 +0530

    Add project files.

---
+++ Web-API-Versioning.API/CountriesData.cs
@@ -0,0 +1,30 @@
+﻿using Web_API_Versioning.API.Models.Domain;
+
+namespace Web_API_Versioning.API
+{
+    public class CountriesData
+    {
+        public static List<Country> Get()
+        {
+            var countries = new[]
+            {
+                new Country { Id = 1, Name = "India" },
+                new Country { Id = 2, Name = "United States" },
+                new Country { Id = 3, Name = "Canada" },
+                new Country { Id = 4, Name = "United Kingdom" },
+                new Country { Id = 5, Name = "Australia" },
+                new Country { Id = 6, Name = "Germany" },
+                new Country { Id = 7, Name = "France" },
+                new Country { Id = 8, Name = "Japan" },
+                new Country { Id = 9, Name = "China" },
+                new Country { Id = 10, Name = "Brazil" }
+            };
+
+            return countries.Select(c=> new Country
+            {
+                Id = c.Id,
+                Name = c.Name
+            }).ToList();
+        }
+    }
+}
---
+++ Web-API-Versioning.API/Models/Dto/CountryDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Web_API_Versioning.API.Models.Dto
+{
+    public class CountryDtoV1
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class CountryDtoV2
+    {
+        public int Id { get; set; }
+        public string CountryName { get; set; }
+    }
+}
